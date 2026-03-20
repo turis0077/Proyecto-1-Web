ce8d7122854ac0d04ac1ce7393b4c279c1d486a7
turis <turiscruz@gmail.com>
2026-03-19 18:48:05 -0600

    chore: add api.js and ui.js modules

---
+++ scripts/api.js
@@ -0,0 +1,12 @@
+const API_BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+export const fetchPosts = async () => {
+    try {
+        const response = await fetch(`${API_BASE_URL}/posts`);
+        if (!response.ok) throw new Error('Error al cargar publicaciones');
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
